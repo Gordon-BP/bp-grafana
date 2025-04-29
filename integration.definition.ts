310b55c9106d1c05d4212ed15dec5334072017b4
Gordon-BP <gordynumberone@protonmail.com>
2025-04-29 19:26:11 +0800

    version bump

--- integration.definition.ts
+++ integration.definition.ts
@@ -5,7 +5,7 @@ export default new IntegrationDefinition({
   name: integrationName,
   title: "Grafana Cloud Integration",
   description: "Sends metric events to Grafana Cloud via HTTP for visualization",
-  version: '1.2.1',
+  version: '1.3.0',
   readme: 'hub.md',
   icon: 'icon.svg',
   configuration: {
