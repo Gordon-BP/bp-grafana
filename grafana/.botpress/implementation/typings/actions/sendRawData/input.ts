fafd1957162d6afd3c66cba069cf8c80a51015c2
Gordon-BP <gordynumberone@protonmail.com>
2025-04-29 19:24:15 +0800

    rm grafana dir

--- grafana/.botpress/implementation/typings/actions/sendRawData/input.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-/* tslint:disable */
-// This file is generated. Do not edit it manually.
-
-export type Input = {
-  /** The data string to send to Grafana. No whitespace. */
-  data: string;
-};
--- grafana/.botpress/implementation/typings/events/metricSent.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-/* tslint:disable */
-// This file is generated. Do not edit it manually.
-
-export type MetricSent = {
-  /** The Id of the event sent to Grafana */
-  id: string;
-};
